9a51440a1c171ad85d12b30afa68c9fe2a4119d1
Jake Buri <jburi@mail.bradley.edu>
2019-10-04 18:53:53 -0500

    first commit

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,51 @@
+﻿/*
+* Jake Buri
+* EnemyX.cs
+* Assignment 6
+* Enemy AI and physics
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		GameObject spawnManager = GameObject.Find("Spawn Manager");
+		SpawnManagerX enemySpeed = spawnManager.GetComponent<SpawnManagerX>();
+		speed = enemySpeed.enemySpeed;
+		playerGoal = GameObject.Find("Goals/Player Goal");
+		enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
